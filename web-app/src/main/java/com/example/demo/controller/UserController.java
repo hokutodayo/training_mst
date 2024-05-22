662015932d77a04764b07c6b3e137cfd6cf4c595
hokutodayo <hokutodayo@gmail.com>
2024-05-22 23:35:14 +0900

    DBの値を取得して、一覧画面に表示

--- web-app/src/main/java/com/example/demo/controller/UserController.java
+++ web-app/src/main/java/com/example/demo/controller/UserController.java
@@ -1,23 +1,28 @@
 package com.example.demo.controller;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import com.example.demo.entity.User;
+import com.example.demo.service.UserService;
+
 @Controller
 public class UserController {
+
+	@Autowired
+	UserService userService;
 
 	@GetMapping("/user/list")
 	public String displayList(Model model) {
 
-		model.addAttribute("id", 100);
-		model.addAttribute("name", "サンプル太郎");
-		model.addAttribute("address", "東京都新宿区1-2-3");
-		model.addAttribute("phone", "090-1234-1234");
-		model.addAttribute("updateDate", LocalDateTime.now());
+		List<User> userList = userService.searchAll();
+		model.addAttribute("userList", userList);
 
 		return "user/list";
 	}
