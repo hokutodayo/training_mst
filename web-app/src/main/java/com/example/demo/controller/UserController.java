c1058448eae84223dbb174bf111628865bf3b6e8
hokutodayo <hokutodayo@gmail.com>
2024-05-22 23:55:42 +0900

    新規登録画面の作成と、データ登録

--- web-app/src/main/java/com/example/demo/controller/UserController.java
+++ web-app/src/main/java/com/example/demo/controller/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.demo.entity.User;
 import com.example.demo.service.UserService;
@@ -35,4 +36,20 @@ public class UserController {
 		return "user/detail";
 	}
 
+	@GetMapping("/user/add")
+	public String displayAdd(Model model) {
+
+		User user = new User();
+		model.addAttribute("user", user);
+
+		return "user/add";
+	}
+
+	@PostMapping("/user/create")
+	public String createUser(User user, Model model) {
+
+		userService.createUser(user);
+
+		return "redirect:/user/list";
+	}
 }
