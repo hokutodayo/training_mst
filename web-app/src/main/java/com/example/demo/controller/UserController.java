d5374709b0f2634b1400bc3a22b6f2fe83899d0e
hokutodayo <hokutodayo@gmail.com>
2024-05-22 23:23:06 +0900

    詳細画面の作成と、一覧画面からの遷移

--- web-app/src/main/java/com/example/demo/controller/UserController.java
+++ web-app/src/main/java/com/example/demo/controller/UserController.java
@@ -5,6 +5,7 @@ import java.time.LocalDateTime;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class UserController {
@@ -19,6 +20,20 @@ public class UserController {
 		model.addAttribute("updateDate", LocalDateTime.now());
 
 		return "user/list";
+	}
+
+	@GetMapping("/user/{id}")
+	public String displayDetail(@PathVariable Long id, Model model) {
+
+		model.addAttribute("id", id);
+		model.addAttribute("name", "サンプル太郎");
+		model.addAttribute("address", "東京都新宿区1-2-3");
+		model.addAttribute("phone", "090-1234-1234");
+		model.addAttribute("updateDate", LocalDateTime.now());
+		model.addAttribute("createDate", LocalDateTime.now());
+		model.addAttribute("deleteDate", null);
+
+		return "user/detail";
 	}
 
 }
